eae2e39c520100ff978812b0e489b1b3f4cab28b
TommPalm <tompal2003@gmail.com>
2026-01-29 20:57:07 +0100

    inizio conversione a mobile

--- Assets/OGGETTI/scripts/movimento.cs
+++ Assets/OGGETTI/scripts/movimento.cs
@@ -12,16 +12,40 @@ public class Movimento : MonoBehaviour
         vel = 0.025f; 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        /*gestione input movimento*/
+
+        /*gestione input movimento pc
         float inputX = Input.GetAxisRaw("Horizontal");
         mov.x = inputX * vel;
         float inputY = Input.GetAxisRaw("Vertical");
-        mov.y = inputY * vel;
+        mov.y = inputY * vel;*/
         transform.Translate(mov);
+        
+        //rb.linearVelocity = mov;
+    }
 
+    /*funzioni gestione input movimento tramite tasti per mobile*/
+    public void UpButton()
+    {
+        mov.y = vel;
+    }
+    public void DownButton()
+    {
+        
+    }
+    public void LeftButton()
+    {
+       
+    }
+    public void RightButton()
+    {
+        
+    }
+    public void ferma()
+    {
+        mov.y = 0;
+        mov.x = 0;
     }
 
 }
